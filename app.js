8d40fed944836918975f8feadaacf52ea703062f
kaberaarthur <arthurkabera@gmail.com>
2025-06-30 10:17:23 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3002; // Use port from environment variable
+
+app.get('/', (req, res) => {
+  res.send('Hello from your Node.js app! This is the homepage.');
+});
+
+// You can add more routes here
+app.get('/about', (req, res) => {
+    res.send('This is the about page.');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
